c8be7d589fbbc0520cea1ae3e74beefa0f2ae0fd
Chris Mann <chrispmann@gmail.com>
2022-11-01 10:26:36 -0700

    commit tree 141d13808f6835ab991e468785c3846f937022e5

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -43,6 +43,16 @@ class BirdsController < ApplicationController
       render json: { error: "Bird not found" }, status: :not_found
     end
   end
+  def destroy
+    bird = Bird.find_by(id: params[:id])
+    if bird
+      bird.destroy
+      head :no_content
+    else
+      render json: {error: "Bird not found"}, status: not_found
+    end
+  end
+
 
   private
 
